f0d780fdbc8c8521ca25f23347a46f0e6e18e3d3
bibhash1996 <bibhash1996@gmail.com>
2025-01-22 13:50:09 +0530

    chat box implementation

--- src/components/AIChatButton.tsx
+++ src/components/AIChatButton.tsx
@@ -1,17 +1,18 @@
 "use client";
 
 import { Bot } from "lucide-react";
-// import { useState } from "react";
+import { useState } from "react";
+import AIChatBox from "./AIChatBox";
 
 export default function AIChatButton() {
-  // const [chatBoxOpen, setChatBoxOpen] = useState(false);
+  const [chatBoxOpen, setChatBoxOpen] = useState(false);
 
   return (
     <>
-      <button onClick={() => {}}>
+      <button onClick={() => setChatBoxOpen(true)}>
         <Bot size={24} />
       </button>
-      {/* <AIChatBox open={chatBoxOpen} onClose={() => setChatBoxOpen(false)} /> */}
+      <AIChatBox open={chatBoxOpen} onClose={() => setChatBoxOpen(false)} />
     </>
   );
 }
